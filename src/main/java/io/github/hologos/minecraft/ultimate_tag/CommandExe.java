885d78b4f99a6e32a97e6a021549fea954de3ec9
Jiří Málek <jiri.malek@gmail.com>
2022-04-08 12:10:17 +0200

    Decompiled Willbob2004's minecraft_ultimate_tag-v1.0 plugin.

---
+++ src/main/java/io/github/hologos/minecraft/ultimate_tag/CommandExe.java
@@ -0,0 +1,45 @@
+package io.github.hologos.minecraft.ultimate_tag;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandExe implements CommandExecutor {
+    public Main plugin;
+
+    public CommandExe(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (label.equalsIgnoreCase("tag")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("[Minecraft Ultimate Tag] This command can only be used by the player.");
+                return true;
+            }
+
+            Player player = (Player) sender;
+            if (args.length == 1) {
+                if (args[0].equalsIgnoreCase("nextGame")) {
+                    this.plugin.nextGame(player);
+                    return true;
+                }
+
+                if (args[0].equalsIgnoreCase("help")) {
+                    this.plugin.helpMenu(player);
+                    return true;
+                }
+            }
+
+            if (args.length == 3 && args[0].equalsIgnoreCase("start")) {
+                this.plugin.startGame(args[1], args[2], player);
+                return true;
+            }
+
+            this.plugin.helpMenu(player);
+        }
+
+        return false;
+    }
+}
